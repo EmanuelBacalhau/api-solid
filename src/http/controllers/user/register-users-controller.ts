aa13b03bff18fdafb0069af3add2759447db31cc
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-17 22:18:54 -0300

    feat: add controller for user register

---
+++ src/http/controllers/user/register-users-controller.ts
@@ -0,0 +1,39 @@
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUserUseCase } from '@/use-cases/user/register-users-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function registerUserController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUserRepository = new PrismaUserRepository()
+    const registerUserUseCase = new RegisterUserUseCase(prismaUserRepository)
+
+    await registerUserUseCase.execute({
+      name,
+      email,
+      password,
+    })
+
+    return reply.status(201).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message,
+      })
+    }
+
+    return reply.status(500).send() // TODO: fix me
+  }
+}
