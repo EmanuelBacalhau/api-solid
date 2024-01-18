9d85bd03c698b90761379df9ae4f2406a4e131da
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-17 22:18:27 -0300

    feat: add use case for register user

---
+++ src/use-cases/user/register-users-use-case.ts
@@ -0,0 +1,24 @@
+import { UserRepository } from '@/repositories/interfaces/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from '../errors/user-already-exists-error'
+
+interface RegisterUserUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUserUseCase {
+  constructor(private registerUserRepository: UserRepository) {}
+  async execute({ email, name, password }: RegisterUserUseCaseRequest) {
+    const emailInUse = await this.registerUserRepository.findByEmail(email)
+
+    if (emailInUse) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const password_hash = await hash(password, 6)
+
+    await this.registerUserRepository.create({ email, name, password_hash })
+  }
+}
