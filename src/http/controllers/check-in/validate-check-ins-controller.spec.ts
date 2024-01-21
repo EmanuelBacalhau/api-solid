30f25ac9ee38e22dd9513665bb8c2472d2e39c7f
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-01-20 23:22:37 -0300

    refactor: Refactoring unit test to accept verify user role

--- src/http/controllers/check-in/validate-check-ins-controller.spec.ts
+++ src/http/controllers/check-in/validate-check-ins-controller.spec.ts
@@ -14,7 +14,7 @@ describe('Check-ins Metrics controller (e2e)', () => {
   })
 
   it('should be able to get the count check-ins', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const gym = await prisma.gym.create({
       data: {
